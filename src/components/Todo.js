2c8117feca4e27cd9656aeecedddfe9b5502c9da
jahidhasan6484 <jahidhasan6484@gmail.com>
2021-06-09 00:39:31 +0600

    add bootstrap

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -11,36 +11,42 @@ const Todo = () => {
         <div className="main-div">
             <div className="child-div">
                 <figure>
-                    <figcaption>Add Your List Here</figcaption>
+                    <figcaption className="text-center title">To Do List</figcaption>
                 </figure>
 
-
                 <div className="addItems">
-                    <input type="text" placeholder="Add Items" value={inputData} onChange={(event) => setInputData(event.target.value)} />
-                    <i className="fa fa-plus add-btn" onClick={() => dispatch(addTodo(inputData), setInputData(''))}></i>
+                    <input type="text" className="form-control" placeholder="Add items" value={inputData} onChange={(event) => setInputData(event.target.value)} />
+                    <i className="fa fa-plus button" onClick={() => dispatch(addTodo(inputData), setInputData(''))}></i>
                 </div>
 
-                <div className="showItems">
-                    {
-                        list.map((elem) => {
-                            return (
-                                <div className="eachItem" key={elem.id}>
-                                    <h3>{elem.data}</h3>
-                                    <div className="todo-btn">
-                                        <i className="fa fa-trash-alt add-btn" title="Delete Item" onClick={() => dispatch(deleteTodo(elem.id))}></i>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
+                <div className="mt-2">
+                    <ol class="list-group list-group-numbered items">
+                        {
+                            list.map((elem) => {
+                                return (
 
+                                    <li class="list-group-item d-flex justify-content-between align-items-start" key={elem.id}>
+                                        <div class="ms-2 me-auto">
+                                            <div class="fw-bold">{elem.data}</div>
+                                        </div>
+                                        <i className="fa fa-trash-alt button" title="Delete Item" onClick={() => dispatch(deleteTodo(elem.id))}></i>
+                                    </li>
+
+                                )
+                            })
+                        }
+
+                    </ol>
                 </div>
 
-                <div className="showItems">
-                    <button className="btn effect04" data-sm-link-text="remove All" onClick={() => dispatch(removeTodo())}>
-                        <span>check list</span>
-                    </button>
-                </div>
+                {
+                    list.length > 1 && <div className="showItems">
+                        <button className="button mt-2 btn btn-outline-danger btn-sm" data-sm-link-text="remove All" onClick={() => dispatch(removeTodo())}>
+                            <span>Remove All</span>
+                        </button>
+                    </div>
+                }
+
 
             </div>
         </div>
