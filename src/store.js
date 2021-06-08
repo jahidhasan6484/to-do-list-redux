27a2c45d32cced8fdd89f2b46f991ac263c91a2b
jahidhasan6484 <jahidhasan6484@gmail.com>
2021-06-08 23:17:35 +0600

    add action added

--- src/store.js
+++ src/store.js
@@ -1,11 +1,6 @@
-import React from 'react';
+import {createStore} from 'redux';
+import rootReducer from './reducers/index';
 
-const store = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
